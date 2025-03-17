310fa988bbb17ef8ed455dcd484170f7d634196a
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2025-03-16 21:51:05 -0300

    build: deployment to gh-pages

--- javascripts/site.js
+++
@@ -1,6 +0,0 @@
-import { Application } from "@hotwired/stimulus"
-
-import HomeController from "./controllers/home_controller.js"
-
-window.Stimulus = Application.start()
-Stimulus.register("home", HomeController)
