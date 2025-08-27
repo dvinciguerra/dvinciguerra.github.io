c0505bf7e3db0d3a8f83212d38361763c8530655
dvinciguerra <daniel.vinciguerra@bivee.com.br>
2025-08-27 01:18:41 +0000

    deploy: update site 2025-08-27 01:18:41 UTC

--- javascripts/site-ff03f7d1.js
+++ javascripts/site-ff03f7d1.js
@@ -1,6 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 
-import HomeController from "./controllers/home_controller.js"
+import HomeController from "controllers/home_controller-25895d8b.js"
 
 window.Stimulus = Application.start()
 Stimulus.register("home", HomeController)
